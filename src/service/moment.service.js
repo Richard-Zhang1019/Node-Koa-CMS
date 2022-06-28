02d95092672563f8d426f6817b1b86d42d95e7fa
zxy <1501109542@qq.com>
2022-06-28 20:19:29 +0800

    fixd

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -1,13 +1,5 @@
 const connection = require('../app/database');
 
-// const sqlFragment = `
-//   SELECT 
-//     m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
-//     JSON_OBJECT('id', u.id, 'name', u.name) author
-//   FROM moment m
-//   LEFT JOIN user u ON m.user_id = u.id
-// `
-
 class MomentService {
   async create(userId, content) {
     const statement = `INSERT INTO moment (content, user_id) VALUES (?, ?);`;
@@ -74,17 +66,6 @@ class MomentService {
     return result;
   }
 
-  async hasLabel(momentId, labelId) {
-    const statement = `SELECT * FROM moment_label WHERE moment_id = ? AND label_id = ?`;
-    const [result] = await connection.execute(statement, [momentId, labelId]);
-    return result[0] ? true: false;
-  }
-
-  async addLabel(momentId, labelId) {
-    const statement = `INSERT INTO moment_label (moment_id, label_id) VALUES (?, ?);`;
-    const [result] = await connection.execute(statement, [momentId, labelId]);
-    return result;
-  }
 }
 
 module.exports = new MomentService();
