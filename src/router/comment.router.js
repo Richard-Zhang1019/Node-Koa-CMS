3142b989940aa8adf72afcb4f00b99a1348e4390
zhangxueyu <1501109542@qq.com>
2022-08-31 22:17:34 +0800

    feat:comment route

--- src/router/comment.router.js
+++ src/router/comment.router.js
@@ -1,5 +1,15 @@
 const Router = require('koa-router');
 
+const {
+  verifyAuth
+} = require('../middleware/auth.middleware')
+
+const {
+  create
+} = require('../controller/moment.controller')
+
 const commentRouter = new Router({prefix: '/comment'});
+
+commentRouter.post('/', verifyAuth, create)
 
 module.exports = commentRouter;
