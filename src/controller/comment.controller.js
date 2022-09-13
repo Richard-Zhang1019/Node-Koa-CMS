41b6c97b30f0c6ca8523e44ffea7794abe2e042a
zhangxueyu <1501109542@qq.com>
2022-09-13 22:15:27 +0800

    feat

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -1,7 +1,13 @@
 class CommentController {
   async create(ctx, next) {
+    ctx.body = '发表评论成功';
+
+    const { momentId, content } = ctx.request.body;
+    const { id } = ctx.user; 
+
     
+
   }
 }
 
-module.exports = CommentController
+module.exports = new CommentController();
