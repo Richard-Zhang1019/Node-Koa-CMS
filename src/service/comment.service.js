b9153698b41c30a0f2e42883b969b083035b04e7
zhangxueyu <1501109542@qq.com>
2022-09-15 22:09:09 +0800

    feat

--- src/service/comment.service.js
+++ src/service/comment.service.js
@@ -1,6 +1,10 @@
+const connection = require('../app/database')
+
 class CommentService {
-  async create(commentId, content, userId) {
+  async create(comentId, content, userId) {
     const statement = `INSERT INTO comment (content, moment_id, user_id) VALUES (?, ?, ?);)`;
+    const [result] = connection.execute(statement, [content, comentId, userId]);
+    return result;
   }
 }
 
