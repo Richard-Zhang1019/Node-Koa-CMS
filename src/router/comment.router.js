f03e5dd09a14a12d92aea747fbe51a8425a067dd
zhangxueyu <1501109542@qq.com>
2022-09-18 22:04:17 +0800

    feat

--- src/router/comment.router.js
+++ src/router/comment.router.js
@@ -14,4 +14,6 @@ const commentRouter = new Router({prefix: '/comment'});
 commentRouter.post('/', verifyAuth, create)
 commentRouter.post('/reply', verifyAuth, reply)
 
+
+
 module.exports = commentRouter;
